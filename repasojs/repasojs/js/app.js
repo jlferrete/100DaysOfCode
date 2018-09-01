335671d62d03814da4d61c2c491712528d87f76c
Jlferrete <joseluisferrete@gmail.com>
2018-09-01 21:45:37 +0200

    Beginning React 16 projects. Instalation and JavaScript review.

--- repasojs/repasojs/js/app.js
+++ repasojs/repasojs/js/app.js
@@ -1,3 +1,7 @@
+/*
+*** LECCION 9
+*/
+
 // Crear variables
 
 // Variables con var
@@ -24,9 +28,65 @@
 // console.log('fuera del if', musica);
 
 // Scope con let
-const musica = 'Rock';
-if(musica){
-    const musica = 'Grunge';
-    console.log('dentro del if: ', musica);
-}
-console.log('fuera del if', musica);
+// const musica = 'Rock';
+// if(musica){
+//     const musica = 'Grunge';
+//     console.log('dentro del if: ', musica);
+// }
+// console.log('fuera del if', musica);
+
+/*
+*** LECCION 10
+*/
+
+// // Template Strings
+// const nombre = 'Jose Luis';
+// const trabajo = 'Desarrollador Web';
+
+// //Concatenar javascript
+// console.log('Nombre: '+nombre + ', Trabajo: '+ trabajo);
+// console.log(`Nombre: ${nombre} , Trabajo: ${trabajo}`);
+
+// //Concatenar con múltiples líneas
+// const contenedorApp = document.querySelector('#app');
+// // let html = '<ul>' +
+// //                 '<li> Nombre: ' + nombre + '</li>' +
+// //                 '<li> Trabajo: ' + trabajo + '</li>' +
+// //             '</ul>';
+
+// let html = `
+//             <ul>
+//                 <li>Nombre: ${nombre} </li>
+//                 <li>Trabajo: ${trabajo} </li>
+//             </ul>
+//         `;
+        
+// contenedorApp.innerHTML = html;
+
+/*
+*** LECCION 11
+*** Diferencias entre function Declaration y function Expression
+*/
+
+// //Creando una funcion
+// function saludar( nombre ){
+//     console.log('Bienvenido ' + nombre);
+// }
+
+// saludar('Juan');
+// saludar('Alejandra');
+// saludar('Miguel');
+
+// //Function Declaration
+// function saludar(){
+//     console.log('Bienvenido ');
+// }
+// saludar();
+
+// //Function expression
+// const cliente = function(nombreCliente) {
+//     console.log('Mostrando datos del cliente: ' + nombreCliente);
+// }
+
+// cliente('Juan');
+
