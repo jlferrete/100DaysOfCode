68db6068c57e8427460cfc2ed948ff54d171701c
jlferrete <joseluisferrete@gmail.com>
2018-09-03 15:19:14 +0200

    Timeline publications method

--- curso-mean-social/api/controllers/publication.js
+++ curso-mean-social/api/controllers/publication.js
@@ -37,7 +37,40 @@ function savePublication(req, res){
 
 }
 
+function getPublications(req, res){
+	var page = 1;
+	if(req.params.page){
+		page = req.params.page;
+	}
+
+	var itemsPerPage = 4;
+
+	Follow.find({user: req.user.sub}).populate('followed').exec((err, follows) => {
+		if(err) return res.status(500).send({message: 'Error al devolver el seguimiento'});
+
+		var follows_clean = [];
+
+		follows.forEach((follow) => {
+			follows_clean.push(follow.followed);
+		});
+
+		Publication.find({user: {"$in": follows_clean}}).sort('-created_at').populate('user').paginate(page, itemsPerPage, (err, publications, total) => {
+			if(err) return res.status(500).send({message: 'Error al devolver publicaciones'});
+
+			if(!publications) return res.status(404).send({message: 'No hay publicaciones'});
+
+			return res.status(200).send({
+				total_items: total,
+				pages: Math.ceil(total/itemsPerPage),
+				page: page,
+				publications
+			})
+		});
+	});
+}
+
 module.exports = {
 	probando,
-	savePublication
+	savePublication,
+	getPublications
 }
--- curso-mean-social/api/routes/publication.js
+++ curso-mean-social/api/routes/publication.js
@@ -10,5 +10,6 @@ var md_upload = multipart({ uploadDir: './uploads/publications'});
 
 api.get('/probando-pub', md_auth.ensureAuth, PublicationController.probando);
 api.post('/publication', md_auth.ensureAuth, PublicationController.savePublication);
+api.get('/publications/:page?', md_auth.ensureAuth, PublicationController.getPublications);
 
 module.exports = api;
