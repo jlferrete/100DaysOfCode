2637798b03dc0f098b90b89084c96848167c5721
Jose Luis Ferrete <joseluisferrete@gmail.com>
2018-09-10 17:48:26 +0200

    Select methods done

--- php-sql+oop/index.php
+++ php-sql+oop/index.php
@@ -14,7 +14,61 @@
 
         $mysql = new mysqli("localhost", "developer", "Studium2018;", "boludatienda");
 
-        var_dump($mysql);
+        $resultado = $mysql->query("SELECT * FROM clientes");
+
+        #mysqli_result
+        // print_r($resultado->fetch_all());
+        // print_r($resultado->fetch_all(MYSQLI_ASSOC)); UTIL
+        // print_r($resultado->fetch_assoc());
+        // print_r($resultado->fetch_object());
+
+        
+        /*
+        ** MÉTODO 1
+
+        $tmp = $resultado->fetch_all(MYSQLI_ASSOC);
+
+        foreach ($tmp as $fila) {
+            echo "{$fila['nombre']} {$fila['apellidos']} ({$fila['edad']})<br>";
+        }
+        
+        */
+
+        /*
+        ** MÉTODO 2
+
+        while($fila = $resultado->fetch_assoc()){
+            echo "{$fila['nombre']} {$fila['apellidos']} ({$fila['edad']})<br>";
+        }
+
+        */
+
+        /*
+        ** MÉTODO 3
+        while($fila = $resultado->fetch_object()){
+            echo "{$fila->nombre} {$fila->apellidos} ({$fila->edad})<br>";
+        }
+
+        */
+
+        $tmp = $resultado->fetch_all(MYSQLI_ASSOC);
+
+        echo "<table style='width:100%;'>";
+        foreach ($tmp as $fila) {
+            echo "<tr>
+                        <td>{$fila['id_clientes']}</td>
+                        <td>{$fila['nombre']}</td>
+                        <td>{$fila['apellidos']}</td>
+                        <td>{$fila['provincia']}</td>
+                        <td>{$fila['edad']}</td>
+                        <td>{$fila['telefono']}</td>
+                        <td>{$fila['email']}</td>
+                    
+                  </tr>";
+        }
+
+        echo "</table>";
+
     ?>
     
 </body>
