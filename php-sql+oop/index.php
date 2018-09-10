283d08f6ade621d956b8d9c9b27d9287367a9e37
Jose Luis Ferrete <joseluisferrete@gmail.com>
2018-09-10 14:07:20 +0200

    Initial config DB connection

---
+++ php-sql+oop/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+</head>
+<body style="padding:15px;">
+
+    <h1>Programa para tiendas</h1>
+    <hr>
+
+    <?php
+
+        $mysql = new mysqli("localhost", "developer", "Studium2018;", "boludatienda");
+
+        var_dump($mysql);
+    ?>
+    
+</body>
+</html>
